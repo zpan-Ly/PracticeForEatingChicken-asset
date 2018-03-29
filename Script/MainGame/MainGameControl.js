27478e21c4ab8a73492a21c7d97f9cf7dd45732b
zpan_Ly <630838897@qq.com>
2018-03-29 22:50:55 +0800

    none

--- Script/MainGame/MainGameControl.js
+++ Script/MainGame/MainGameControl.js
@@ -38,7 +38,7 @@ cc.Class({
         },
         music: cc.AudioClip,
         interval: 1,
-        crosschairSpeed: 300,
+        crosschairSpeed: 20,
         crosschairRadius: 100
     },
 
@@ -48,8 +48,15 @@ cc.Class({
         this._chickenNum = 0;
         this._gameOver = false;
         this._score = 0;
-        this._crosschairDir = 0;//0代表准星没有移动
-        this._locChangeButtonPreviousLocation = this.locChangeButton.node.getPosition();//触摸点固定位置
+        this._crosschairDir = 0;
+        this._crosschairDir2 = 0;
+        this._isMovingCrossChair = false;
+        this._movingSca = 0;
+        var radius = 50;
+        var widget = 100;
+        this._locChangeButtonPreviousLocation = cc.p(widget-cc.winSize.width/2+radius,widget-320+radius);//触摸点固定位置
+
+        this.crosschair.node.setPosition(cc.p(0,0));
 
         cc.audioEngine.play(this.music,true,Global.musicVolume);
 
@@ -87,6 +94,11 @@ cc.Class({
         }
 
         if (this.blood.node.opacity > 1) this.blood.node.opacity -= 1;//血量图透明度降低
+
+        if(!this._isMovingCrossChair) return;
+    
+        this.crosschair.node.x += this.crosschairSpeed*Math.cos(this._crosschairDir)*dt*this._movingSca*this._crosschairDir2;
+        this.crosschair.node.y += this.crosschairSpeed*Math.sin(this._crosschairDir)*dt*this._movingSca;
     },
 
     updateScore: function(){
@@ -142,6 +154,13 @@ cc.Class({
             newBlood.removeFromParent();
         },2);
     },
+    setPosFromWin: function(pos){//定高下的策略
+        var scal = 960 / cc.winSize.width;
+        pos.x *= scal;
+        pos.x -= 480;
+        pos.y -= 320;
+        return pos;
+    },
 
     setTouchEvent: function(){
         /*this.node.on(cc.Node.EventType.MOUSE_DOWN,function (event) {//枪声
@@ -154,37 +173,60 @@ cc.Class({
         var startPos = cc.p(0,0);
 
         sprite.node.on(cc.Node.EventType.TOUCH_START, function(e){  
-            startPos = cc.p(e.touch._point._x,e.touch._point._y);
+            startPos = this._locChangeButtonPreviousLocation;
+            this._isMovingCrossChair = true;
             return true;  
         }.bind(this), this);  
 
         sprite.node.on(cc.Node.EventType.TOUCH_MOVE, function(e){  
             var move_x = e.touch._point.x - e.touch._prevPoint.x;  
             var move_y = e.touch._point.y - e.touch._prevPoint.y;  
-            var tempx =  sprite.node.x + move_x;  
-            var tempy = sprite.node.y + move_y;  
 
-            var dist = Math.sqrt((tempx - this._locChangeButtonPreviousLocation.x)*
-            (tempx - this._locChangeButtonPreviousLocation.x)+
-            (tempy - this._locChangeButtonPreviousLocation.y)*
-            (tempy - this._locChangeButtonPreviousLocation.y));
+            var scal = 960 / cc.winSize.width;
+            var tempx =  sprite.node.x + move_x*scal;  
+            var tempy = sprite.node.y + move_y*scal;  
+
+            var dist = Math.sqrt((tempx - startPos.x)*
+            (tempx - startPos.x)+
+            (tempy - startPos.y)*
+            (tempy - startPos.y));
             if(dist > this.crosschairRadius){//触摸点的活动范围是有限的
                 var scaleOfDist = this.crosschairRadius / dist;
                 sprite.node.setPosition(
-                    this._locChangeButtonPreviousLocation.x+(tempx-this._locChangeButtonPreviousLocation.x)*scaleOfDist,
-                    this._locChangeButtonPreviousLocation.y+(tempy-this._locChangeButtonPreviousLocation.y)*scaleOfDist
+                    startPos.x+(tempx-startPos.x)*scaleOfDist,
+                    startPos.y+(tempy-startPos.y)*scaleOfDist
                 );
             }else{
                 sprite.node.setPosition(tempx,tempy);
+            }
+            this._movingSca = dist/100;
+            if(this._movingSca>1) this._movingSca=1;
+
+            tempx = (tempx-startPos.x);
+            tempy = (tempy-startPos.y);
+
+            if(-0.00001<tempx && tempx <0.00001) tempx = 0.00001;
+            if(-0.00001<tempy && tempy <0.00001) tempy = 0.00001;
+
+            var tempz = Math.sqrt(tempx*tempx+tempy*tempy);
+            if(-0.00001<tempz && tempz <0.00001){
+                return;
+            }else{
+                this._crosschairDir = Math.asin(tempy/tempz);
+                this._crosschairDir2 = tempx>0?1:-1;
             }
         }.bind(this), this ); 
 
         sprite.node.on(cc.Node.EventType.TOUCH_END, function(e){  
             sprite.node.setPosition(this._locChangeButtonPreviousLocation);
+            this._isMovingCrossChair = false;
+            this._movingSca = 0;
         }.bind(this), this );  
 
         sprite.node.on(cc.Node.EventType.TOUCH_CANCEL, function(e){  
             sprite.node.setPosition(this._locChangeButtonPreviousLocation);
+            this._isMovingCrossChair = false;
+            this._movingSca = 0;
         }.bind(this), this );  
     }
 });
